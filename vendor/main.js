37e9987c5a7fb1c32b2eaa0c085f54dadf804494
OrnellaSablic <ornellasablic@gmail.com>
2023-02-09 15:05:20 -0300

    Últimos detalles del proyecto

--- vendor/main.js
+++ vendor/main.js
@@ -1,16 +1,16 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        500:{
-            items:2
+        500: {
+            items: 2
         },
-        900:{
-            items:3
+        900: {
+            items: 3
         }
     }
 })
